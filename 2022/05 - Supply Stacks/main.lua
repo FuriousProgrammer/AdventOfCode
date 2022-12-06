26331ec8b0e232c8ebea1a595c8dc36a36925bf4
Adam Albee <furiousprogrammer@gmail.com>
2022-12-06 17:10:24 -0600

    2022 Day 5 - use initial config from input.txt, instead of hard-coding it.

--- 2022/05 - Supply Stacks/main.lua
+++ 2022/05 - Supply Stacks/main.lua
@@ -1,33 +1,49 @@
--- TODO: read initial configuration from input.txt
-local stacks = {
-	{'H', 'T', 'Z', 'D'};
-	{'Q', 'R', 'W', 'T', 'G', 'C', 'S'};
-	{'P', 'B', 'F', 'Q', 'N', 'R', 'C', 'H'};
-	{'L', 'C', 'N', 'F', 'H', 'Z'};
-	{'G', 'L', 'F', 'Q', 'S'};
-	{'V', 'P', 'W', 'Z', 'B', 'R', 'C', 'S'};
-	{'Z', 'F', 'J'};
-	{'D', 'L', 'V', 'Z', 'R', 'H', 'Q'};
-	{'B', 'H', 'G', 'N', 'F', 'Z', 'L', 'D'};
-}
+local stacks = {}
+do
+	-- there *should* always be spaces padding out the initial config diagram,
+	-- but this code is written assuming trailing whitepsace has been truncated
 
-local stacks2 = {
-	{'H', 'T', 'Z', 'D'};
-	{'Q', 'R', 'W', 'T', 'G', 'C', 'S'};
-	{'P', 'B', 'F', 'Q', 'N', 'R', 'C', 'H'};
-	{'L', 'C', 'N', 'F', 'H', 'Z'};
-	{'G', 'L', 'F', 'Q', 'S'};
-	{'V', 'P', 'W', 'Z', 'B', 'R', 'C', 'S'};
-	{'Z', 'F', 'J'};
-	{'D', 'L', 'V', 'Z', 'R', 'H', 'Q'};
-	{'B', 'H', 'G', 'N', 'F', 'Z', 'L', 'D'};
-}
+	-- read all lines until first empty line, storing them in reverse order
+	local lines = {}
+	for line in io.lines("input.txt") do
+		if line == "" then break end
+		table.insert(lines, 1, line)
+	end
 
+	-- topmost line is the stack labels, always 1..n with no gaps so we only
+	-- need to grab the final, largest, value to get the number of stacks
+	local numstacks = table.remove(lines, 1):match("(%d+)%s*$")
+	for i = 1, numstacks do
+		stacks[i] = {}
+	end
+
+	-- finally, extract the characters stored in each stack, bottom-to-top
+	for _, line in ipairs(lines) do
+		for i = 0, numstacks-1 do
+			local c = line:sub(i*4 + 2, i*4 + 2)
+			
+			if c and c ~= ' ' then
+				table.insert(stacks[i+1], c)
+			end
+		end
+	end
+end
+
+-- deep copy of stacks for part 2
+local stacks2 = {}
+for i, v in pairs(stacks) do
+	stacks2[i] = {}
+	for j, n in pairs(v) do
+		stacks2[i][j] = n
+	end
+end
+
+
+local tmp = {}
 for line in io.lines("input.txt") do
 	n, src, dst = line:match("move (%d+) from (%d+) to (%d+)")
 	n, src, dst = tonumber(n) or 0, tonumber(src), tonumber(dst)
 
-	local tmp = {}
 	for i = 1, n do
 		table.insert( stacks[dst], table.remove(stacks[src]) )
 
@@ -35,16 +51,17 @@ for line in io.lines("input.txt") do
 	end
 
 	for i = 1, n do
-		table.insert( stacks2[dst], table.remove(tmp) )
+		table.insert( stacks2[dst], table.remove(tmp) ) -- double reversal for part 2
 	end
-
 end
+
 
 io.write("Part 1: ")
 for _, s in ipairs(stacks) do
 	io.write(s[#s])
 end
 io.write('\n')
+
 
 io.write("Part 2: ")
 for _, s in ipairs(stacks2) do
